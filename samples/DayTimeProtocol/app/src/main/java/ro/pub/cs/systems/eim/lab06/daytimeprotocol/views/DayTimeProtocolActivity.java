6eb16ea7bd0d29e86155bdac74aa3fa6ff04988d
Dragoș Niculescu <dragos.niculescu@cs.pub.ro>
2025-11-19 17:11:54 +0200

    Cleaned up example

--- samples/DayTimeProtocol/app/src/main/java/ro/pub/cs/systems/eim/lab06/daytimeprotocol/views/DayTimeProtocolActivity.java
+++ samples/DayTimeProtocol/app/src/main/java/ro/pub/cs/systems/eim/lab06/daytimeprotocol/views/DayTimeProtocolActivity.java
@@ -1,6 +1,6 @@
 package ro.pub.cs.systems.eim.lab06.daytimeprotocol.views;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
